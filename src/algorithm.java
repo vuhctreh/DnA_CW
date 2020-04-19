f95b464a733be6528900289f99450a0207482cec
Victor <victor.lecardonnel@hotmail.fr>
2020-04-19 19:55:55 +0100

    Finished base case

--- src/algorithm.java
+++ src/algorithm.java
@@ -1,17 +1,19 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-//TODO make elevator pick people up on the way down too
+import java.util.stream.Collectors;
 
 public class algorithm {
-    public static LiftMovement direction;
+    public static LiftMovement direction = LiftMovement.UP;
     public static int floors = 6;
     public static int current_floor = 0;
-    public static int max_load = 500;
+    public static int max_load = 150;
 
     static List<Integer> stops = new ArrayList<>();
     static List<person> people_in_elevator = new ArrayList<>();
-    static lift elevator = new lift(direction, stops, "Idle", 0, people_in_elevator);
+    static lift elevator = new lift(LiftMovement.UP, stops, "Idle", 0, people_in_elevator);
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -25,63 +27,86 @@ public class algorithm {
         boolean ArePeopleWaiting = Building.stream().anyMatch(lift_floor::getPeopleWaiting);
 
         while (ArePeopleWaiting) {
-            for (current_floor = 0; current_floor < floors; current_floor++) {
-                System.out.println("Lift is going up; current floor is " + current_floor);
-                if (Building.get(current_floor).getPeopleWaiting() && Building.get(current_floor).getPopulation().size() > 0) {
-                    for (int i = 0; i < Building.get(current_floor).getPopulation().size(); i++) {
-                        System.out.println("There are " + Building.get(current_floor).getPopulation().size() + " people waiting on floor " + current_floor);
-                        if (elevator.getCurrent_load() > max_load) {
-                            current_floor++;
-                            break;
-                        }
-                        else {
-                            people_in_elevator.add(Building.get(current_floor).getPopulation().get(i));
-                            stops.add(Building.get(current_floor).getPopulation().get(i).getDestination_floor());
-                            elevator.setCurrent_load(elevator.getCurrent_load() + Building.get(current_floor).getPopulation().get(i).getWeight());
-                            Building.get(current_floor).getPopulation().remove(Building.get(current_floor).getPopulation().get(i));
-                            System.out.println("Current elevator load = " + elevator.getCurrent_load());
+            if (direction == LiftMovement.UP) {
+                for (current_floor = 0; current_floor < floors; current_floor++) {
+                    if(current_floor > floors - 1) {
+                        break;
+                    }
+                    if (Building.get(current_floor).getPopulation().isEmpty())
+                        Building.get(current_floor).setPeopleWaiting(false);
 
-                            if (Building.get(current_floor).getPopulation().size() == 0) {
-                                Building.get(current_floor).setPeopleWaiting(false);
+                    if (Building.get(current_floor).getPeopleWaiting()) {
+                        final Iterator<person> iterator = Building.get(current_floor).getPopulation().iterator();
+                        while (iterator.hasNext()) {
+                            final person person = iterator.next();
+                            if (person.getWeight() + elevator.getCurrent_load() > max_load)
+                                break;
+                            else {
+                                stops.add(person.getDestination_floor());
+                                elevator.setCurrent_load(person.getWeight() + elevator.getCurrent_load());
+                                elevator.getPeople().add(person);
+                                iterator.remove();
                             }
                         }
+                        List<Integer> listWithoutDuplicates = stops.stream().distinct().collect(Collectors.toList());
+                        elevator.setStops(stops);
+                        TimeUnit.SECONDS.sleep(2);
                     }
-                    elevator.setStops(stops);
-                    System.out.println("Current planned stops are: " + elevator.getStops());
-                    System.out.println("Passenger(s) picked up on floor " + current_floor);
-                    TimeUnit.SECONDS.sleep(2);
-                    if (Building.get(current_floor).getPopulation().size() == 0) {
-                        Building.get(current_floor).setPeopleWaiting(false);
-                    }
+                    LiftDropOffLoop();
+                    direction = LiftMovement.IDLE;
+                    isAtTop = true;
                 }
-                LiftDropOffLoop();
-                direction = LiftMovement.IDLE;
-                isAtTop = true;
             }
-            if (direction == LiftMovement.IDLE && isAtTop) {
+            if (direction == LiftMovement.IDLE && isAtTop)
                 direction = LiftMovement.DOWN;
-            }
-            if (direction == LiftMovement.DOWN) {
-                for (current_floor = (floors - 1); current_floor > -1; current_floor--) {
-                    System.out.println("Lift is going down; current floor is " + current_floor);
+            if (direction == LiftMovement.DOWN){
+                for (current_floor = (floors - 1); current_floor > -1; current_floor--){
+                    if (current_floor == 0){
+                        isAtTop = false;
+                        direction = LiftMovement.IDLE;
+                        break;
+                    }
+                    if (Building.get(current_floor).getPopulation().isEmpty())
+                        Building.get(current_floor).setPeopleWaiting(false);
+
+                    if (Building.get(current_floor).getPeopleWaiting()) {
+                        final Iterator<person> iterator = Building.get(current_floor).getPopulation().iterator();
+                        while (iterator.hasNext()) {
+                            final person person = iterator.next();
+                            if (person.getWeight() + elevator.getCurrent_load() > max_load)
+                                break;
+                            else {
+                                stops.add(person.getDestination_floor());
+                                elevator.setCurrent_load(person.getWeight() + elevator.getCurrent_load());
+                                elevator.getPeople().add(person);
+                                iterator.remove();
+                                }
+                            }
+                        List<Integer> listWithoutDuplicates = stops.stream().distinct().collect(Collectors.toList());
+                        elevator.setStops(stops);
+                        TimeUnit.SECONDS.sleep(2);
+                    }
                     LiftDropOffLoop();
                 }
-            } ArePeopleWaiting = Building.stream().anyMatch(lift_floor::getPeopleWaiting);
+            }
+            if (direction == LiftMovement.IDLE && !isAtTop)
+                direction = LiftMovement.UP;
+            ArePeopleWaiting = Building.stream().anyMatch(lift_floor::getPeopleWaiting);
         }
     }
 
     public static void LiftDropOffLoop() {
-        for (int i = 0; i < stops.size(); i++) {
+
+        for (int i = 0; i < elevator.getStops().size(); i++) {
             if (elevator.getStops().get(i).equals(current_floor)) {
-                int y = 0;
-                for (int x = 0; x < elevator.getPeople().size(); x++) {
-                    if (elevator.getPeople().get(x).getDestination_floor() == current_floor) {
-                        elevator.setCurrent_load(elevator.getCurrent_load() - elevator.getPeople().get(x).getWeight());
-                        elevator.getPeople().remove(elevator.getPeople().get(x));
-                        y++;
+                final Iterator<person> iterator = elevator.getPeople().iterator();
+                while (iterator.hasNext()) {
+                    final person person = iterator.next();
+                    if(person.getDestination_floor() == current_floor){
+                        elevator.setCurrent_load(elevator.getCurrent_load() - person.getWeight());
+                        iterator.remove();
                     }
                 }
-                System.out.println(y + " Passenger(s) dropped off on floor " + current_floor);
                 elevator.getStops().remove(elevator.getStops().get(i));
             }
         }
